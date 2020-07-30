75be22d6e0eef19253808ca986e21d38a6b370db
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-07-30 20:00:55 +0200

    output version

--- dist/index.js
+++ dist/index.js
@@ -487,6 +487,25 @@ exports.debug = debug; // for test
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -496,16 +515,20 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __webpack_require__(470);
-const github_1 = __webpack_require__(469);
+const core_1 = __importStar(__webpack_require__(470));
+const github_1 = __importDefault(__webpack_require__(469));
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            let version = github_1.context.ref.replace('refs/tags/release/', '');
+            let version = github_1.default.context.ref.replace('refs/tags/release/', '');
             if (version.startsWith('v')) {
                 version = version.substr(1);
             }
+            core_1.default.setOutput('version', version);
         }
         catch (error) {
             core_1.setFailed(error.message);
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import {setFailed} from '@actions/core'
-import {context} from '@actions/github'
+import core, {setFailed} from '@actions/core'
+import github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    let version: string = context.ref.replace('refs/tags/release/', '')
+    let version: string = github.context.ref.replace('refs/tags/release/', '')
 
     if (version.startsWith('v')) {
       version = version.substr(1)
     }
+
+    core.setOutput('version', version)
   } catch (error) {
     setFailed(error.message)
   }
