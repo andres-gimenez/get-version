2a6ab019084782560e1eb675cc86dc97f5b2afc6
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-07-30 20:15:07 +0200

    context

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import core, {setFailed} from '@actions/core'
-import github from '@actions/github'
+import {context} from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    let version: string = github.context.ref.replace('refs/tags/release/', '')
+    let version: string = context.ref.replace('refs/tags/release/', '')
 
     if (version.startsWith('v')) {
       version = version.substr(1)
