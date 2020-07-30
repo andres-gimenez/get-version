750a9ee80613302843a1f740fb65a2b4e3934e9d
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-07-30 21:19:10 +0200

    Debug.

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,23 @@ import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
+    //refs/heads/release
+    core.debug('1')
     let version: string = github.context.ref.replace('refs/tags/release/', '')
+    core.debug('2')
 
     if (version.startsWith('v')) {
       version = version.substr(1)
     }
-
-    core.setOutput('version', version)
-
+    core.debug('3')
     core.debug('Version: ${version}')
+    core.debug('4')
+    core.setOutput('version', version)
+    core.debug('5')
   } catch (error) {
+    core.debug('6')
     core.setFailed(error.message)
+    core.debug('7')
   }
 }
 
