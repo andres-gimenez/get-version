58b387420c19b9c72df2a6fe5535e415b6c07ef8
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-07-31 14:26:25 +0200

    Delete traze.

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function run(): Promise<void> {
   try {
     //refs/heads/release
     let version = "";
-    core.debug("1");
+
     if (github.context.ref.startsWith("refs/heads")) {
       core.debug("Headers");
       //const refs = github.context.ref.split('/');
@@ -17,21 +17,15 @@ async function run(): Promise<void> {
       //version = github.context.ref.replace('refs/tags/release/', '');
       version = "1.3.4";
     }
-    core.debug("2");
 
     if (version.startsWith("v")) {
       version = version.substr(1);
     }
-    core.debug("3");
     core.debug(`Version: ${version}`);
-    core.debug("4");
     core.setOutput("version", version);
     core.info(`Version: ${version}`);
-    core.debug("5");
   } catch (error) {
-    core.debug("6");
     core.setFailed(error.message);
-    core.debug("7");
   }
 }
 
